e350839f67a22d975d02277c1c9e51af4e70c38d
Rahul Chand <rahulchand@Rahuls-MacBook-Air.local>
2023-08-05 08:04:01 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -5,15 +5,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
 }
-
-module.exports = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-
-    return config;
-  },
-};
 
 module.exports = nextConfig
