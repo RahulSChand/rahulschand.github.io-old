e71b49bb3fd49be3d10989561843dca1746c8f85
Rahul Chand <rahulchand@Rahuls-MacBook-Air.local>
2023-08-05 07:51:23 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -7,5 +7,13 @@ const nextConfig = {
   },
 }
 
+module.exports = {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+
+    return config;
+  },
+};
 
 module.exports = nextConfig
