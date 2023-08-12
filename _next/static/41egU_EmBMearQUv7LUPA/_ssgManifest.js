0922d16dcb7a226365d208f9666f1511e6fecb64
RahulSChand <RahulSChand@users.noreply.github.com>
2023-08-12 18:04:31 +0000

    deploy: 3b36621944ab11a3c04abbb74bfe886368d61ef7

---
+++ _next/static/41egU_EmBMearQUv7LUPA/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblogs\u002Fpages\u002Fposts\u002F[id]","\u002Fblogs\u002Fpages\u002Fposts\u002Flanding","\u002Fblogs\u002Fpages\u002FpostsPersonal\u002F[id]","\u002Fblogs\u002Fpages\u002FpostsPersonal\u002Flanding"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/dldNxO4gr-rlwQp0MoUG_/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblogs\u002Fpages\u002Fposts\u002F[id]","\u002Fblogs\u002Fpages\u002Fposts\u002Flanding"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
