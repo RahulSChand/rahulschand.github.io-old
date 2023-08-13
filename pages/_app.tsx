7aa19f2f7265d71053d7f55abf05a59e2633b66a
Rahul Chand <rahulchand@Rahuls-MacBook-Air.local>
2023-08-14 05:00:15 +0530

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
+import Script from "next/script";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Script async src="https://www.googletagmanager.com/gtag/js?id=G-SMFBL8NKP9"></Script>
+<Script>
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SMFBL8NKP9');
+  `}
+</Script>
+  <Component {...pageProps} />
+  </>
 }
