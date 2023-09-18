d32c4632937e4880bbe1dab8e3dd5ac09ac79b0c
Rahul Chand <rahulchand@Rahuls-MacBook-Air.local>
2023-09-18 14:27:05 +0530

    fixed mistake

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,8 @@ export default function Home() {
                 <br>
                 </br>
                 Right now working on the <span className="underline">current</span> thing. 
-                <br></br>Check out my new <a className="underline font-semibold" href="https://github.com/RahulSChand/gpu_poor">github project</a> if you want to see how GPU poor you are are.
+                <br></br>Check out my new <a className="underline font-semibold" target="_blank"
+                rel="noopener noreferrer" href="https://github.com/RahulSChand/gpu_poor">github project</a>.
                 {/* <br></br>Previously also spent time @ IISC, IIRS & Arcesium. */}
                 
                 <br></br>
